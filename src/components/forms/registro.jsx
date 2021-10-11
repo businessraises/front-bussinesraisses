03db9f49111249bdb6d2790e5a60769b3f618f97
businessraises <businessraises@gmail.com>
2021-10-11 15:40:43 -0500

    tratando de hacerlos formularios

--- src/components/forms/registro.jsx
+++ src/components/forms/registro.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import {
   Grid,
   Paper,
@@ -13,11 +14,15 @@ import {
   Card,
 } from "@mui/material";
 
-const styuleCustomeSingUpCard = {
-  marginTop: "10%",
-  paddingTop: "10%",
-  paddingBottom: "20px"
+const styleCustomeSingUpCard = {
+  backgroundColor: "#f9f9f9",
+  marginTop: '20px',
+  paddingTop: '20px',
 };
+
+const styleCustomeForm = {
+
+}
 
 const RegistroForm = () => {
   const { user, setUser, registrar } = useContext(UserContext);
@@ -32,9 +37,30 @@ const RegistroForm = () => {
   };
 
   return (
-    <Card variant="elevation" sx={{ ...styuleCustomeSingUpCard }}>
-        
-      
+    <Card
+      variant="elevation"
+      sx={{ ...styleCustomeSingUpCard }}
+      justifyContent="center"
+    >
+      <Grid container justifyContent="center">
+          <Grid item xs={12} >
+            <AccountCircleIcon 
+                sx={{
+                    fontSize: '5.9em',
+                    paddingLeft: '40%',
+                    paddingBottom: 2
+
+                }}
+            />
+          </Grid>
+        <FormControl>
+          <InputLabel htmlFor="my-input">Email address</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
+      </Grid>
     </Card>
   );
 };
--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -7,42 +7,39 @@ import {makeStyles} from '@mui/styles'
 import RegistroForm from '../components/forms/registro'
 import Login from '../components/forms/login'
 
-const useStyle = makeStyles({
-    root:{
-        flexGrow: 1
-    }
-})
 
 
 const Welcome =  () =>  {
     
-    const clasess = useStyle()
     return (
-       <div className={clasess.root}>
-           <Grid container>
+       <div sx={{ flexGrow: 1 }}>
+           <Grid container >
                <Grid item xs={12}>
                 <Navbar />
                </Grid>
-               <Grid item
-                xm={12}
-                sm={6}
-                md={6}
-                lg={6}
-                xl={6}
-               >
-                    {/* Form Registro */}
-                <RegistroForm />
+               <Grid container spacing={3}>
+                    <Grid item
+                        xs={12}
+                        sm={12}
+                        md={6}
+                        lg={6}
+                        xl={6}
+                        
+                    >
+                            {/* Form Registro */}
 
-               </Grid>
-               <Grid item
-                xm={12}
-                sm={6}
-                md={6}
-                lg={6}
-                xl={6}
-               >
-                   {/* Presentacion */}
-                    
+                    </Grid>
+                    <Grid item
+                        xs={12}
+                        sm={12}
+                        md={6}
+                        lg={6}
+                        xl={6}
+                    >
+                        {/* Presentacion */}
+                        <RegistroForm /> 
+                    </Grid>
+
                </Grid>
            </Grid>
        </div>
